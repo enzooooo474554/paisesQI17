e7f1866aeb9fcf2910ad2818c48e833c87221a6a
enzooooo474554 <168134587+enzooooo474554@users.noreply.github.com>
2024-05-02 21:30:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+paises =[]
+
+pais = input("Lemão, que país você quer conhecer? ")
+paises.append(pais)
+
+paises.append(input("Qual o segundo país? "))
+paises.append(input("Qual o terceiro país? "))
+
+print("Minha lista de países é : ",paises)
